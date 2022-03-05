74a7e95444f2b405b79c7a94d0d135cc1522ec32
Mani Movassagh <mani.mgh@gmail.com>
2022-03-05 19:35:05 +0100

    done

--- src/test/java/com/github/com/CustomerAddressApplicationTests.java
+++ src/test/java/com/github/com/CustomerAddressApplicationTests.java
@@ -1,15 +1,40 @@
 package com.github.com;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.github.com.entities.Address;
+import com.github.com.entities.Customer;
+import com.github.com.repositories.CustomerRepository;
 
 @SpringBootTest
 class CustomerAddressApplicationTests {
-
+	@Autowired
+	CustomerRepository repository;
 	
 	
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testSaveCustomer() {
+		
+		Customer customer = new Customer();
+		customer.setId(1);
+		customer.setName("mani");
+		customer.setFamily("Movassagh");
+		Address address = new Address();
+		address.setCity("Mainz");
+		address.setCountry("Germany");
+		address.setState("RHLP");
+		address.setZopcode("55120");
+		address.setStreet("Rheinallee");
+		customer.setAddress(address);
+		repository.save(customer);
+		
+		
 	}
 	
 	
